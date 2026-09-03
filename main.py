6ebbf3f8eb101db785c6708c37e4199a429f121a
mohammedalmaqtari98-png <mohammed.almaqtari98@gmail.com>
2026-09-03 19:05:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
+import os
 import telebot
-from flask import Flask
-from threading import Thread
 
-# سيرفر خفيف لإبقاء الخدمة نشطة على Render
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is Alive and Running 24/7!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
-
-# التوكن المباشر للبوت
-TOKEN = "8646517225:AAFJNmFnmycaCiZAeLd33OZrXGs8MgZAdzg"
+# إبقاء التوكن مخفياً عن GitHub
+TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
-
-SOURCE_CHANNELS = [
-    "art_of_book",
-    "riwayattime",
-    "katabatakatabata",
-    "books2023",
-    "million_2026"
-]
-
-TARGET_CHANNELS = [
-    "@rafiq_words_group",
-    "@wordscomp"
-]
-
 @bot.channel_post_handler(func=lambda message: True)
 def auto_forward(message):
     if message.chat.username and message.chat.username.lower() in SOURCE_CHANNELS:
